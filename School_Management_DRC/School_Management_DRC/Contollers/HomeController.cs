0d1e089c39d6a62e82435c179f18351079a80957
Felly <fellyka@gmail.com>
2020-07-18 21:17:24 +0200

    Index view added

---
+++ School_Management_DRC/School_Management_DRC/Contollers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace School_Management_DRC.Contollers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
